36a94cc962299627ab16dfdc156f744bf2dd56eb
shashikant88093 <shashi94goswami@gmail.com>
2023-12-30 21:45:53 +0530

    Refactor login and signup buttons to use TypeScript event types

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -56,7 +56,9 @@ export default function LoginPage() {
                     />
                 </div>
             </div>
-            <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={(e) => onLogin(e)}>
+            <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => onLogin(e)}
+            >
                 Login
             </button>
             <Link href="/signup">
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -93,7 +93,7 @@ export default function SignupPage() {
                 {/* signup button in center */}
                 <div className="w-full px-3">
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="button"
-                        onClick={(e) => onSignup(e)}
+                      onClick={(e: React.MouseEvent<HTMLButtonElement>) => onSignup(e)}
                         disabled={buttonDisabled}
                     >
                         Signup
